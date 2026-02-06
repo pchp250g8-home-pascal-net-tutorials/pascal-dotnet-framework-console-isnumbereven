2d5b5269be5a7c2894dd3dc2db04dd1d2b242bbd
Alexander <pchp250g8@tutamail.com>
2026-02-06 15:31:30 +0300

    Created project IsNumberEven

---
+++ IsNumberEven.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var ulNum:UInt64 := 0;
+  Console.Clear();
+  Console.WriteLine('Input an integer positive number:');
+  var strNum := Console.ReadLine();
+  UInt64.TryParse(strNum, ulNum);
+  if(ulNum mod 2 = 0) then
+    Console.WriteLine('The number is even')
+  else
+     Console.WriteLine('The number is odd');
+  Console.Read();
+end.
